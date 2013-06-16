358ebf071a7438b9f9f4fbdae32e07eddf861343
csaez <cesarte@gmail.com>
2013-06-16 04:25:45 -0400

    new is_icon()

--- rigicon/icon.py
+++ rigicon/icon.py
@@ -1,18 +1,24 @@
 from wishlib.si import si, SIWrapper
 
+DEFAULT_DATA = {"icon": "Null", "connect": None,
+                "priority": 0, "size": 1.0,
+                "_wirecolorr": 1.0, "_wirecolorg": 0.882, "_wirecolorb": 0.0,
+                "posx": 0.0, "posy": 0.0, "posz": 0.0,
+                "rotx": 0.0, "roty": 0.0, "rotz": 0.0,
+                "sclx": 1.0, "scly": 1.0, "sclz": 1.0,
+                "behaviour": "srt"}
+PROPERTY_NAME = "RigIcon_Data"
+APPLYTRANSFORM_COMPOUND = "csRigIcon__ApplyTransform"
+CONNECT_COMPOUND = "csRigIcon__ConnectionLine"
+
+
+def is_icon(obj):
+    if obj.Type == "crvlist" and obj.Properties(PROPERTY_NAME) is not None:
+        return True
+    return False
+
 
 class Icon(SIWrapper):
-    DEFAULT_DATA = {"icon": "Null", "connect": None,
-                    "priority": 0, "size": 1.0,
-                    "_wirecolorr": 1.0, "_wirecolorg": 0.882, "_wirecolorb": 0.0,
-                    "posx": 0.0, "posy": 0.0, "posz": 0.0,
-                    "rotx": 0.0, "roty": 0.0, "rotz": 0.0,
-                    "sclx": 1.0, "scly": 1.0, "sclz": 1.0,
-                    "behaviour": "srt"}
-    PROPERTY_NAME = "RigIcon_Data"
-    APPLYTRANSFORM_COMPOUND = "csRigIcon__ApplyTransform"
-    CONNECT_COMPOUND = "csRigIcon__ConnectionLine"
-
     @classmethod
     def create(cls, name="rig_icon"):
         obj = si.ActiveSceneRoot.AddNurbsCurve()
@@ -23,17 +29,17 @@ class Icon(SIWrapper):
     def __new__(cls, obj):
         if obj.Type == "crvlist":
             return SIWrapper.__new__(cls, obj)
-        print "ERROR:", obj, "type isnt crvlist."
+        print "ERROR:", obj, "type isnt an icon object."
 
     def __init__(self, obj):
-        super(Icon, self).__init__(obj, self.PROPERTY_NAME)
+        super(Icon, self).__init__(obj, PROPERTY_NAME)
         self.wirecolorr = property(lambda: self._get_color("wirecolorr"),
                                    lambda x: self._set_color("wirecolorr", x))
         self.wirecolorg = property(lambda: self._get_color("wirecolorg"),
                                    lambda x: self._set_color("wirecolorg", x))
         self.wirecolorb = property(lambda: self._get_color("wirecolorb"),
                                    lambda x: self._set_color("wirecolorb", x))
-        for k, v in self.DEFAULT_DATA.iteritems():
+        for k, v in DEFAULT_DATA.iteritems():
             if not hasattr(self, k):
                 setattr(self, k, v)
 
