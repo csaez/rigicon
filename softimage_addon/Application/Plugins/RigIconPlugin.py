1923776d8c210af512e389248e4a2d023980c2b0
csaez <cesarte@gmail.com>
2013-06-25 02:59:11 -0400

    new RigIconReloader, just for dev purposes...

--- softimage_addon/Application/Plugins/RigIconPlugin.py
+++ softimage_addon/Application/Plugins/RigIconPlugin.py
@@ -1,3 +1,4 @@
+import sys
 from wishlib.si import log, C, show_qt
 
 
@@ -10,6 +11,7 @@ def XSILoadPlugin(in_reg):
     in_reg.RegisterCommand("RigIconLibrary", "RigIconLibrary")
     in_reg.RegisterCommand("RigIconEditor", "RigIconEditor")
     in_reg.RegisterCommand("RigIcon", "RigIcon")
+    in_reg.RegisterCommand("RigIconReloader", "RigIconReloader")
     in_reg.RegisterFilter("RigIcon", C.siFilter3DObject)
     return True
 
@@ -20,28 +22,36 @@ def XSIUnloadPlugin(in_reg):
 
 
 def RigIconLibrary_Execute():
-    log("csRigIconLibrary_Execute called", C.siVerbose)
+    log("RigIconLibrary_Execute called", C.siVerbose)
     from rigicon.layout.library_gui import RigIconLibrary
     show_qt(RigIconLibrary)
     return True
 
 
 def RigIconEditor_Execute():
-    log("csRigIconEditor_Execute called", C.siVerbose)
+    log("RigIconEditor_Execute called", C.siVerbose)
     from rigicon.layout.editor_gui import RigIconEditor
     show_qt(RigIconEditor)
     return True
 
 
 def RigIcon_Execute():
-    log("csRigIconLibrary_Execute called", C.siVerbose)
+    log("RigIcon_Execute called", C.siVerbose)
     from rigicon.icon import Icon
     Icon.create()
     return True
 
 
+def RigIconReloader_Execute():
+    log("RigIconReloader_Execute called", C.siVerbose)
+    for k in sys.modules.keys():
+        if k.startswith("rigicon"):
+            del sys.modules[k]
+    return True
+
+
 def RigIcon_Match(in_ctxt):
-    log("csRigIconFilter_Match called", C.siVerbose)
+    log("RigIconFilter_Match called", C.siVerbose)
     obj = in_ctxt.GetAttribute("Input")
     from rigicon.icon import is_icon
     return is_icon(obj)
